71d6b99dd572976595ead2790ceff44e512796c4
Bruce Zheng <bruce.c.zheng@gmail.com>
2024-04-26 22:49:27 -0700

    Change font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Serif:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
