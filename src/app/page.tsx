0fc74d55d9afa60c63628c1e54640d76012185fa
Bruce Zheng <bruce.c.zheng@gmail.com>
2024-05-08 23:39:06 -0500

    RSVP button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
 import { ReactNode } from "react";
 import styles from "./page.module.css";
-import Button from "./components/button/Button";
+
+const RSVP_LINK = "https://forms.gle/BrtkuYS6Tqhhvi1g8";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.content}>
-        <div>Bruce's <TextShadow>30th</TextShadow></div>
-        <div><TextShadow>Cusco</TextShadow>, Peru</div>
-        <div>Sep <TextShadow>6-8</TextShadow>, 2024</div>
-        {/* <Button>RSVP</Button> */}
+        <div className={styles.contentText}>
+          <div>Bruce's <TextShadow>30th</TextShadow></div>
+          <div><TextShadow>Cusco</TextShadow>, Peru</div>
+          <div>Sep <TextShadow>6-8</TextShadow>, 2024</div>
+        </div>
+        <a className={styles.button} href={RSVP_LINK}>RSVP</a>
       </div>
     </main>
   );
